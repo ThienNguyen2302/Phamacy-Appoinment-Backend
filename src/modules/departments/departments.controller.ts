550add85532ef04261d4ae6a34d84b7f11161d05
dodinhphu <dodinhphu316@gmail.com>
2024-02-27 00:03:43 +0700

    [feat] add role for departments

--- src/modules/departments/departments.controller.ts
+++ src/modules/departments/departments.controller.ts
@@ -2,7 +2,9 @@ import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { DepartmentsService } from './departments.service';
 import { CreateDepartmentDto } from './dto/create-department.dto';
 import { UpdateDepartmentDto } from './dto/update-department.dto';
+import { RolesAndGuard } from '../../decorators/roleAndGuard.decorator';
 
+@RolesAndGuard(['admin'])
 @Controller('departments')
 export class DepartmentsController {
   constructor(private readonly departmentsService: DepartmentsService) {}
